b82d182e8adda38c440b26402a66090c780e13c3
Kabir Goel <kabirgoel.kg@gmail.com>
2024-06-10 20:09:14 -0700

    More robust URL construction (#291)

--- src/tts/websocket.ts
+++ src/tts/websocket.ts
@@ -150,12 +150,11 @@ export default class WebSocket extends Client {
 	 * @throws {Error} If the WebSocket fails to connect.
 	 */
 	connect() {
-		const url = constructApiUrl(
-			this.baseUrl,
-			`/tts/websocket?cartesia_version=${CARTESIA_VERSION}`,
-			"ws",
-		);
+		const url = constructApiUrl(this.baseUrl, "/tts/websocket", {
+			websocket: true,
+		});
 		url.searchParams.set("api_key", this.apiKey);
+		url.searchParams.set("cartesia_version", CARTESIA_VERSION);
 		const emitter = new Emittery<ConnectionEventData>();
 		this.socket = new PartySocketWebSocket(url.toString());
 		this.socket.onopen = () => {
