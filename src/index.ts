f665f9b80dbe4a5b659047995a0c8415ba95a7c0
Nick Oates <nick@nickoates.com>
2024-06-30 17:10:07 -0700

    Export source

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Cartesia as default } from "./lib";
 export * from "./lib";
 export * from "./types";
 export { default as WebPlayer } from "./tts/player";
+export { default as Source } from "./tts/source";
