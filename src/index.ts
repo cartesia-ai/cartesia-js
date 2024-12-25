4d198434bcd1b8a2c27c218711a2eb81a407fcdc
Deep Singhvi <deep@buildwithfern.com>
2024-12-25 09:43:45 -0500

    chore: export `WebPlayer`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as Cartesia from "./api";
 export { CartesiaClient } from "./wrapper/Client";
+export { WebPlayer } from "./wrapper/WebPlayer";
 export { CartesiaEnvironment } from "./environments";
 export { CartesiaError, CartesiaTimeoutError } from "./errors";
