cbd7cdc127c86cd33c146a27d5a8fc80f2d56260
Charlotte Zhuang <charlotte.zhuang@cartesia.ai>
2026-04-27 16:39:02 -0700

    chore: Fix build

--- src/internal/headers.ts
+++ src/internal/headers.ts
@@ -1,5 +1,8 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+
 import { isReadonlyArray } from './utils/values';
 
 type HeaderValue = string | undefined | null;
